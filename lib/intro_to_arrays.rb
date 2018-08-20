8de38977e4b0083b439285955f77f0ce258c2abf
Nasser Sanou <nassersanou23@gmail.com>
2018-08-20 07:42:17 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,53 @@
+def my_new_array
+  new_array = ["a", "b", "c"]
+end
+
+def instantiate_new_array
+  array = []
+end
+
+def array_with_two_elements
+  array = ["Patience",  "Dedication"]
+end
+
+
+def my_two_array
+  array = ["Patience",  "Dedication"]
+end
+
+
+def taylor_swift
+  array = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+end
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+    array.index(2)
+end
+
+def last_element(array)
+  array[-1]
+end
+
+def south_east_asia
+  countries = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+end
+
+def first_element_with_array_methods(first_country)
+  first_country.first
+end
+
+def last_element_with_array_methods(last_country)
+  last_country.last
+end
+
+def programming_languages
+ languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+end
+
+def length_of_array(programming_languages)
+  programming_languages.size
+end
